d5b4c854c7a62d64b1ba236058635fb987f998d2
Lê Minh Nhật <leminhnhat1326@gmail.com>
2025-09-30 10:29:10 +0700

    first commit

---
+++ js/auth.js
@@ -0,0 +1,5 @@
+function toggleForm(formId) {
+      document.getElementById("loginForm").classList.remove("active");
+      document.getElementById("registerForm").classList.remove("active");
+      document.getElementById(formId).classList.add("active");
+    }
